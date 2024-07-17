5d1e2bb68f0a11b721717f1d83bf2fcbec82f338
Manisha Rana <manisharana02005@gmial.com>
2024-07-17 18:16:33 +0530

    Added delete funtionality

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,12 +1,22 @@
 
 import Todo from "../Todo/Todo";
 
-function TodoList({todos}){
+function TodoList({todos, setTodos}){
+
+    function onDeleteTodo(id){
+      
+        const newTodoList=todos.filter(todo=>todo.id!=id);
+        setTodos(newTodoList);
+        
+    }
     return(
          todos && todos.map((todo)=><Todo 
                                         key={todo.id}
-                                        text={todo.text}
-                                        isFinished={todo.isFinished}/>)
+                                        text={todo.text}                                   
+                                        isFinished={todo.isFinished}
+                                        deleteTodo={()=>onDeleteTodo(todo.id)}
+                                        
+                                        />)
     )
 }
 
