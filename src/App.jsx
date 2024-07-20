3e88b885fc9e94bf589325c50f3d0bf96eacb3e7
Manisha Rana <manisharana02005@gmial.com>
2024-07-20 14:10:36 +0530

    Used context api in the todo list

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import AddTodo from './components/AddTodo/AddTodo'
 import TodoList from './components/TodoList/TodoList'
+import TodoContext from './context/TodoContext'
 
 function App() {
 
@@ -11,15 +12,21 @@ function App() {
     // {id: 2, text:'todo2', isFinished: false}
   ])
 
-  function addTodos(todoText){
-    let nextId=todos.length+1;
-    setTodos([...todos, {id: nextId, isFinished:false, text: todoText}]);
-  }
+  // function addTodos(todoText){
+  //   let nextId=todos.length+1;
+  //   setTodos([...todos, {id: nextId, isFinished:false, text: todoText}]);
+  // }
 
   return (
     <>
-      <AddTodo addTodos={addTodos}/>
-      <TodoList todos={todos} setTodos={setTodos}/>
+    {/*it takes a common store for you and extract it out so that you can exess it from anywhere
+     context api allows you to make a common state or common document that multiple components can simultaneouly read and write 
+    to access it you have to create a context object i.e create a new doc now that doc should be shared bet only a set of components
+    */}
+      <TodoContext.Provider value={{todos, setTodos}}>
+        <AddTodo/>
+        <TodoList/>
+      </TodoContext.Provider>
     </>
   )
 }
--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import TodoContext from "../../context/TodoContext";
 
-function AddTodo({addTodos}){
+function AddTodo(){
 
+    const {todos, setTodos}=useContext(TodoContext);  //to access the document use the hook useContext
     const[todoText, setTodoText]=useState('');
+    
 
     return(
         <>
@@ -12,7 +15,8 @@ function AddTodo({addTodos}){
             
             />
             <button onClick={()=>{
-                addTodos(todoText);
+                let nextId=todos.length+1;
+                setTodos([...todos, {id: nextId, text:todoText, isFinished:false}])
                 setTodoText('');
             }}>Submit</button>
         </>
